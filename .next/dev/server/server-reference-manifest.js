210e313dfd306ac2e1c6400dbf4b5d546fce771b
Heidinger <heidinger.sanchez@pucp.edu.pe>
2026-03-10 00:08:49 -0500

    fix: agregar .next a gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Q7Yjb8cAX18EjzqDkiSUf0neoCTFKsGOj9iei7xjsTM=\"\n}"
--- .next/server/chunks/ssr/app_layout_jsx_060703dd._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[98421,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("html",{lang:"es",children:(0,b.jsx)("body",{style:{margin:0,padding:0},children:a})})}a.s(["default",()=>c,"metadata",0,{title:"Brujula Digital",description:"Plataforma de alfabetización digital para docentes"}])}];
-
-//# sourceMappingURL=app_layout_jsx_060703dd._.js.map
